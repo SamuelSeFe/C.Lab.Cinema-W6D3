9601bf2bda00e642fb6f5b7dcd754a8f5abdfcd6
Samuel <samuelsf@hotmail.co.uk>
2022-07-27 13:55:01 +0100

    finished lab with extension

---
+++ models/cinema.js
@@ -0,0 +1,50 @@
+const Cinema = function (films) {
+  this.films = films;
+};
+
+Cinema.prototype.filmTitles = function () {
+  const titles = this.films.map( film => film.title);
+  return titles;
+};
+
+Cinema.prototype.filmByTitle = function (filmTitle) {
+  const byTitle = this.films.filter( film => film.title === filmTitle );
+  return byTitle;
+};
+
+Cinema.prototype.filmByGenre = function (filmGenre) {
+  const byGenre = this.films.filter( film => film.genre === filmGenre);
+  return byGenre;
+};
+
+Cinema.prototype.filmByYear = function (filmYear) {
+  const byYear = this.films.filter( film => film.year === filmYear);
+  return byYear;
+}; 
+
+Cinema.prototype.noFilmThisYear = function (filmYear) {
+  const byYear = this.films.every( film => film.year === filmYear);
+  return byYear;
+};
+
+Cinema.prototype.yesFilmByLength = function (filmLength) {
+  const byLength = this.films.every( film => film.length > filmLength);
+  return byLength;
+};
+
+// Cinema.prototype.filmByLength = function (filmLength) {
+//   const byLength = this.films.filter (film => film.length > 130);
+//   return byLength;
+// };
+
+Cinema.prototype.totalFilmsLength = function () {
+  return this.films.reduce( (runningTotal, film) => { return runningTotal + film.length; 
+  }, 0);
+};
+
+Cinema.prototype.filmsByProperty = function (filmProperty, filmPropertyValue) {
+  const byProperty = this.films.filter( film => film[filmProperty] === filmPropertyValue);
+  return byProperty;
+};
+
+module.exports = Cinema;
